677a837fe3a6fe850ebdd16244fb3ed64e3671a9
hogura88 <hogura88@gmail.com>
2024-05-06 23:17:22 +0900

    修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,8 +8,24 @@
  </head>
  
  <body>
-     <h1>投稿一覧</h1>
-     <p>投稿一覧</p>
+ <table>
+         <tr>
+             <th>ID</th>
+             <th>商品名</th>
+             <th>価格</th>
+             <th>作成日時</th>
+             <th>更新日時</th>
+         </tr>
+         @foreach($posts as $post)
+             <tr>
+                 <td>{{ $post->id }}</td>
+                 <td>{{ $post->product_name }}</td>
+                 <td>{{ $post->price }}</td>
+                 <td>{{ $post->created_at }}</td>
+                 <td>{{ $post->updated_at }}</td>
+             </tr>            
+         @endforeach       
+     </table>
  </body>
  
  </html>
